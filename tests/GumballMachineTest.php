d9950f5b24a58882267062f3c99d294af2f95d6c
ValentinKoolen <44612614+ValentinKoolen@users.noreply.github.com>
2019-11-20 10:50:15 +0100

    Update GumballMachineTest.php

--- tests/GumballMachineTest.php
+++ tests/GumballMachineTest.php
@@ -58,7 +58,7 @@ class GumballMachineTest extends PHPUnit_Framework_TestCase
     }
     public function testInsertP()
     {
-        DropData();
+        setUp();
         $max__id1=$this->gumballMachineInstance->GetLastIDP();
         $this->assertEquals(true,$this->gumballMachineInstance->InsertP($this->gumballMachineInstance->getDB(),$this->nom,$this->prenom,$this->date_naissance,$this->lieu_naissance));
         $this->assertEquals(true,$this->gumballMachineInstance->InsertP($this->gumballMachineInstance->getDB(),$this->nom1,$this->prenom1,$this->date_naissance1,$this->lieu_naissance1)); 
