67fe86255651fd4a263ec1c4ff9b9f5ca7d3aeb1
ValentinKoolen <44612614+ValentinKoolen@users.noreply.github.com>
2019-11-27 09:16:06 +0100

    Update GumballMachine.php

--- GumballMachine.php
+++ GumballMachine.php
@@ -8,9 +8,9 @@ class GumballMachine
 	private $bdd;
 	/* Paramètre de connexion à la base de données*/
 	private $servername="localhost";
-	private $db_name="mydb1"; //a remplir
-	private $db_user="myuser1"; //a remplir
-	private $db_pass="mypassword1"; //a remplir
+	private $db_name="mydb19"; //a remplir
+	private $db_user="myuser19"; //a remplir
+	private $db_pass="mypassword19"; //a remplir
 	
 	
 	function __construct()
